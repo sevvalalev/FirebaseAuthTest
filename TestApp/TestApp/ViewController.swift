7607087a320abaacc2531b4abd8e8b56b3a15f6f
sevvalalev <75833556+sevvalalev@users.noreply.github.com>
2023-09-11 16:54:21 +0300

    Initial commit

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Şevval Alev on 9.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
+
